5615e48cf0e22a8c99801fd897e3ca91c3253c76
seznecetna <seznec_j@etna-alternance.net>
2016-04-11 16:25:43 +0200

    deuxieme commit usb

--- src/fr/demos/icommerce/amazon/Panier.java
+++ src/fr/demos/icommerce/amazon/Panier.java
@@ -4,40 +4,40 @@ import java.util.ArrayList;
 
 public class Panier {
 
-	private ArrayList<Article> listePanier;
+	private ArrayList<Article> listPanier;
 
 	public Panier() {
-		listePanier = new ArrayList<Article>();
+		listPanier = new ArrayList<Article>();
 	}
 
 	public void ajouterArticle(Article article) throws PanierException {
 		if (article.existe()) {
-			this.listePanier.add(article);
+			this.listPanier.add(article);
 			article.decremeter();
 		} else
 			throw new PanierException("Il n'y a plus d'articl en stock");
 	}
 
 	public void retirerArticle(Article article) throws PanierException {
-		if (!listePanier.isEmpty()) {// pour tester son panier vide il y a la
+		if (!listPanier.isEmpty()) {// pour tester son panier vide il y a la
 										// fonction listePanier.isemptu.
-			this.listePanier.remove(article);
+			this.listPanier.remove(article);
 			article.incrementer();
 		} else
 			throw new PanierException("Il n'y a rien dans le panier");
 	}
 
 	public ArrayList<Article> getListePanier() {
-		return listePanier;
+		return listPanier;
 	}
 
 	public void setListePanier(ArrayList<Article> listePanier) {
-		this.listePanier = listePanier;
+		this.listPanier = listePanier;
 	}
 
 	@Override
 	public String toString() {
-		return "Panier [listePanier=" + listePanier + "]";
+		return "Panier [listePanier=" + listPanier + "]";
 	}
 
 }
